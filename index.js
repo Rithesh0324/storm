649db665e6e4b5cfa2da7d26ea0b6ee35d3975f8
Rithesh0324 <ritheshshenoy12@gmail.com>
2026-03-27 04:45:27 +0530

    third commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("hello world");
+
+async function data(){
+    try{
+    let res=await fetch("youtube.com");
+    let data=res.json();
+    return data
+    }catch(err){
+        console.error("error fount in fetching")
+    }
+}
